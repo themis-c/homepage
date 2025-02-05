42fec59cd956c0c8f7b83840ceae0e3d33c35543
Themis_Cocomazzi <themis.cocomazzi@soprasteria.com>
2025-02-05 10:48:29 +0100

    back to simple route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from "react";
-import { Route, Routes, HashRouter } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import AppAppBar from "./components/AppAppBar";
 import Home from "./components/Home";
 function App({ Component, pageProps }) {
   return (
     <div>
       <AppAppBar />
-      <HashRouter basename="/homepage">
-        <Routes>
-          <Route path="/Home" element={<Home />}></Route>
-        </Routes>
-      </HashRouter>
+      <Routes>
+        <Route path="/homepage" element={<Home />}></Route>
+      </Routes>
     </div>
   );
 }
